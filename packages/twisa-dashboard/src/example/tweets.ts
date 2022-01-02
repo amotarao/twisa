7bfa970d69bf7c877923c0c055cde91ead0f7e63
Amon Sawamura <amon.sawamura@gmail.com>
2022-01-02 12:34:44 +0900

    add view

---
+++ packages/twisa-dashboard/src/example/tweets.ts
@@ -0,0 +1,10 @@
+export const tweets = [
+  {
+    id: '1',
+    date: new Date('2021/01/01 00:00:00'),
+    name: 'ユーザー名',
+    screenName: 'ScreenName',
+    body: 'ツイート本文ツイート本文ツイート本文',
+    url: 'https://twitter.com/ScreenName/status/1',
+  },
+];
